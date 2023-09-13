2c1ce2ab303da590fd965b2a6b692759ad842d23
bx-lr <41136019+bx-lr@users.noreply.github.com>
2023-09-13 05:19:26 -0500

    added stack questions

---
+++ questions/stack/evaluate_reverse_polish_notation.py
@@ -0,0 +1,130 @@
+from typing import *
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+
+    def check(self) -> int:
+        '''
+        check the doSomething() function
+        return 0 if tests passed
+        return 1 if tests failed
+        '''
+        arg0 = [["2","1","+","3","*"], ["4","13","5","/","+"], ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]]
+        expected = [9, 6, 22]
+        results = []
+        for a0, exp in zip(arg0, expected):
+            res = self.evalRPN(a0)
+            results.append(res)
+            if res == exp:
+                print(f'\tpassed test: {a0}, expected: {exp}, result: {res}')
+            else:
+                print(f'\t!!!failed test: {a0}, expected: {exp}, result: {res}')
+                return 1
+        if results == expected:
+            print('**** All tests passed ****')
+        return 0
+
+
+def get_difficulty() -> int:
+    '''
+    1 = easy
+    2 = medium
+    3 = hard
+    '''
+    return 2
+
+
+def get_instructions() -> str:
+    ins = '''Evaluate Reverse Polish Notation
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+Example 1:
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+Example 3:
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+Constraints:
+1 <= tokens.length <= 104
+tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+'''
+    return ins
+
+
+def get_solution() -> str:
+    '''
+    Called when check fails
+    should return solution as string
+    '''
+    solution = '''
+Incorrect! Here is the solution:
+
+def evalRPN(self, tokens: List[str]) -> int:
+    stack = []
+    for c in tokens:
+        if c == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif c == '-':
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif c == '/':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(float(b) / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
+
+
+'''
+    return solution
+
+
+def main():
+    sol = Solution()
+    sol.check()
+
+
+if __name__ == '__main__':
+    main()
---
+++ questions/stack/min_stack.py
@@ -0,0 +1,140 @@
+from typing import *
+
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        return
+
+    def pop(self) -> None:
+        return
+
+    def top(self) -> int:
+        return
+
+    def getMin(self) -> int:
+        return
+
+    def check(self) -> int:
+        '''
+        check the class...
+        ["MinStack","push","push","push","getMin","pop","top","getMin"]
+        [[],[-2],[0],[-3],[],[],[],[]]
+
+        Output
+        [null,null,null,null,-3,null,0,-2]
+        '''
+        arg0 = ["MinStack","push","push","push","getMin","pop","top","getMin"]
+        arg1 = [[],[-2],[0],[-3],[],[],[],[]]
+        expected = [None,None,None,None,-3,None,0,-2]
+        results = []
+        for a0, a1, exp in zip(arg0, arg1, expected):
+            if a0 == 'MinStack':
+                results.append(None)
+            elif a0 == 'push':
+                results.append(self.push(a1))
+            elif a0 == 'getMin':
+                results.extend(self.getMin())
+            elif a0 == 'pop':
+                results.append(self.pop())
+            elif a0 == 'top':
+                results.extend(self.top())
+            else:
+                continue
+        if results != expected:
+            print(f'!!!!failed test, expected {expected}, result: {results}')
+            return 1
+        else:
+            print('**** All tests passed ****')
+            return 0
+
+
+def get_difficulty() -> int:
+    '''
+    1 = easy
+    2 = medium
+    3 = hard
+    '''
+    return 2
+
+
+def get_instructions() -> str:
+    ins = '''Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1:
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+'''
+    return ins
+
+
+def get_solution() -> str:
+    '''
+    Called when check fails
+    should return solution as string
+    '''
+    solution = '''
+Incorrect! Here is the solution:
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+'''
+    return solution
+
+
+def main():
+    sol = Solution()
+    sol.check()
+
+
+if __name__ == '__main__':
+    main()
